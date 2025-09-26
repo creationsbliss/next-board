7470993e77f8eada594fff7855880c0ede09235e
Musfiq Rahman <procreationsbliss@gmail.com>
2025-09-26 14:12:07 +0600

    Add chart

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,7 @@
 import { UsersIcon } from "lucide-react";
 
 const Card = () => (
-  <div className="flex w-full gap-4 rounded-sm bg-sidebar-accent p-4">
+  <div className="flex w-full gap-4 rounded-sm bg-sidebar-accent p-4 hover:bg-sidebar-border">
     <div className="flex h-10 w-10 items-center justify-center rounded-full border-1 border-chart-2/40 bg-chart-2/10">
       <UsersIcon size={18} />
     </div>
--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,3 +1,66 @@
-const Chart = () => <div>Chart Component</div>;
+"use client";
+
+import {
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const data = [
+  { name: "Mon", click: 1200, visit: 800 },
+  { name: "Tue", click: 2300, visit: 1500 },
+  { name: "Wed", click: 3400, visit: 2100 },
+  { name: "Thu", click: 2800, visit: 1900 },
+  { name: "Fri", click: 4000, visit: 2400 },
+  { name: "Sat", click: 3700, visit: 2200 },
+  { name: "Sun", click: 3100, visit: 1800 },
+];
+
+const Chart = () => (
+  <div className="h-[350px] rounded-sm bg-sidebar-accent p-4">
+    <h2 className="mb-6 text-xl">Weekly Report</h2>
+    <ResponsiveContainer height="90%" width="100%">
+      <LineChart
+        data={data}
+        height={300}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+        width={500}
+      >
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip
+          contentStyle={{
+            borderRadius: "8px",
+            border: "none",
+            color: "#fff",
+          }}
+          labelStyle={{ fontWeight: "bold", color: "#000" }}
+        />
+        <Legend />
+        <Line
+          dataKey="click"
+          stroke="#009689"
+          strokeDasharray="5 5"
+          type="monotone"
+        />
+        <Line
+          dataKey="visit"
+          stroke="#ffb900"
+          strokeDasharray="3 4 5 2"
+          type="monotone"
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  </div>
+);
 
 export default Chart;
